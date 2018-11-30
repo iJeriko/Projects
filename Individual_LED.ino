997ba82a93ed72769cb75edb172a2abf9fa7a3af
iJeriko <45489282+iJeriko@users.noreply.github.com>
2018-11-30 08:15:21 -0600

    Arduino
    Arduino Projects

---
+++ Individual_LED.ino
@@ -0,0 +1,18 @@
+int REDPIN = 10;
+int r;
+int FADESPEED = 1;
+void setup() {
+pinMode(REDPIN, OUTPUT);
+}
+
+void loop() {
+  for (r = 0; r < 256; r++) { 
+    analogWrite(REDPIN, r);
+    delay(FADESPEED);
+  } 
+   for (r = 255; r > 0; r--) { 
+    analogWrite(REDPIN, r);
+    delay(FADESPEED);
+   }
+
+}
